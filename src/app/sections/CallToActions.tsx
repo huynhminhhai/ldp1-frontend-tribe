c7c4e36f7f33a4fd0956d7aab5e710983fcc4543
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-11-04 19:10:08 +0700

    setup project

---
+++ src/app/sections/CallToActions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CallToActions = () => {
+  return (
+    <div>CallToActions</div>
+  )
+}
+
+export default CallToActions
---
+++ src/app/sections/LogoTikcer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogoTikcer = () => {
+  return (
+    <div>LogoTikcer</div>
+  )
+}
+
+export default LogoTikcer
