842420dda4b90a9984bd84482ada65faa43e0646
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-11-05 11:38:29 +0700

    logo sticker section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import LogoTikcer from "./sections/LogoTikcer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <LogoTikcer />
     </>
   );
 }
--- src/app/sections/LogoTikcer.tsx
+++ src/app/sections/LogoTikcer.tsx
@@ -1,8 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
 
 const LogoTikcer = () => {
   return (
-    <div>LogoTikcer</div>
+    <div className='py-8 md:py-12 bg-white'>
+      <div className='container'>
+        <div className='flex overflow-hidden mask-gradient relative'>
+          <div className='flex gap-14 flex-none animate-marquee whitespace-nowrap'>
+            <Image className='logo-ticker-img' src='/logo-acme.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-quantum.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-echo.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-celestial.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-pulse.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-apex.png' alt='logo' width={250} height={250} />
+          </div>
+          <div className='flex gap-14 flex-none animate-marquee2 whitespace-nowrap absolute top-0 left-14'>
+            <Image className='logo-ticker-img' src='/logo-acme.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-quantum.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-echo.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-celestial.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-pulse.png' alt='logo' width={250} height={250} />
+            <Image className='logo-ticker-img' src='/logo-apex.png' alt='logo' width={250} height={250} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
